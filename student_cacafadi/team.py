3865dcf24a64cdaa8dd81c6e0267809547e91fe2
soohnzzang <74288159+soohnzzang@users.noreply.github.com>
2020-12-15 14:19:38 +0900

    fUGIGI

--- student_cacafadi/team.py
+++ student_cacafadi/team.py
@@ -25,15 +25,47 @@ class O(Unit):
                          att=cls.ATT,
                          arm=cls.ARM,
                          evs=cls.EVS)
+class I(Unit): #뻐기기
+    # X + 1.5Y >= 50
+    HP = 20 # Hit Points (health points)    
+    ATT = 0 # Attack
+    ARM = 36.66  # Armor
+    EVS = 0 # Evasion
+        
+    def __init__(self, team, name, pos):
+        cls = __class__
+        super().__init__(team,
+                         name,
+                         pos,
+                         hp=cls.HP,
+                         att=cls.ATT,
+                         arm=cls.ARM,
+                         evs=cls.EVS)        
        
-
 
 class MyTeam(Team):
     def initialize(self):
-        for i in range(20):
-            unit = O(self, "B-Unit%02d"%(i+1), i)
-            self.units.append(unit)
-            
+        self.units.append(O(self, "Unit1-01", 0))
+        self.units.append(O(self, "Unit1-01", 1))  
+        self.units.append(O(self, "Unit1-01", 2))
+        self.units.append(O(self, "Unit1-01", 3))        
+        self.units.append(O(self, "Unit1-01", 4))        
+        self.units.append(O(self, "Unit1-01", 5))    
+        self.units.append(O(self, "Unit1-01", 6))
+        self.units.append(O(self, "Unit1-01", 7))        
+        self.units.append(O(self, "Unit1-01", 8))   
+        self.units.append(O(self, "Unit1-01", 9))
+        self.units.append(O(self, "Unit1-01", 10))
+        self.units.append(O(self, "Unit1-01", 11))  
+        self.units.append(O(self, "Unit1-01", 12))
+        self.units.append(O(self, "Unit1-01", 13))        
+        self.units.append(O(self, "Unit1-01", 14))        
+        self.units.append(O(self, "Unit1-01", 15))    
+        self.units.append(O(self, "Unit1-01", 16))
+        self.units.append(I(self, "Unit1-01", 17))        
+        self.units.append(I(self, "Unit1-01", 18))   
+        self.units.append(I(self, "Unit1-01", 19))
+        
     def arrange(self, enemy: Team):
         first_unit = self.units[0]
         for i in range(self.num_positions - 1):
